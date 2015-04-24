51bded0b140f18f5023d14447e4bbd7b4612c4ed
whroman <me@walterroman.com>
2015-04-23 21:32:45 -0500

    changing db access creds

--- server/db-connect.js
+++ server/db-connect.js
@@ -3,8 +3,8 @@ var mysql = require('mysql');
 var connect = function () {
     var options = {};
     options.host = 'localhost';
-    options.database = 'al_legals';
-    options.user = 'al_user';
+    options.database = 'alabamalegals';
+    options.user = 'scraper';
     options.password = process.env.HUD_DB_PW;
 
     return mysql.createConnection(options);
