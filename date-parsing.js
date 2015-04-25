8893456c36a37678760cea688cf494de2fd826cf
whroman <me@walterroman.com>
2015-04-24 22:59:05 -0500

    successfully grabing dates from 4 different statements, but not hooked up to db

--- date-parsing.js
+++ date-parsing.js
@@ -5,15 +5,18 @@ parse.date = {};
 parse.date.months = 'January|February|March|April|May|June|July|August|September|October|November|December'
 
 parse.date.strict = function (str) {
-    var re1 = /(?:Alabama\,\son\s)((January|February|March|April|May|June|July|August|September|October|November|December).{0,10}\s20\d\d)/gi;
-    var re2 = /(?:Alabama\,\swithin\sthe\slegal\shours\sof\ssale)(.{0,30}\s20\d\d)/gi;
-    var re3 = /(?:Alabama\,\sduring\sthe\slegal\shours\sof\ssale)(.{0,30}\s20\d\d)/gi;
-    // var re4 = /(?:Alabama\,\sduring\sthe\slegal\shours\sof\ssale\,\son\s)((January|February|March|April|May|June|July|August|September|October|November|December).{0,10}\s20\d\d)/gi;
-    var results1 = re1.exec(str);
-    var results2 = re2.exec(str);
-    var results3 = re3.exec(str);
-    return results1 || results2 || results3;
-}
+    var re1 = /(?:Alabama(?:\,)?(?:.{0,30})?\son\s)(.{0,30}\s20\d\d)/gi;
+    var re2 = /(?:of\ssale(?:\,?\son\s(?:the\s)?)?)(.{0,30}\s20\d\d)|(\d\d\/\d\d\/\d\d)/gi;
+    var re3 = /(?:sale\scontained\sin\sthe\ssaid\smortgage\swill\,\son\s)(.{0,30}\s20\d\d)/gi;
+    var re4 = /(?:between\s11\:00\sA\.M\.\sand\s3\:00\sP\.M\.\son\s)(.{0,30}\s20\d\d)/gi;
+
+    return (
+        re1.exec(str) ||
+        re2.exec(str) ||
+        re3.exec(str) ||
+        re4.exec(str)
+    );
+};
 
 parse.date.postponed = function (str) {
     var re1 = /(?:The\sabove\smortgage\sforeclosure\ssale\shas\sbeen\spostponed\suntil\s)((January|February|March|April|May|June|July|August|September|October|November|December).{0,10}\s20\d\d)/gi;
@@ -21,34 +24,39 @@ parse.date.postponed = function (str) {
     var re3 = /(?:THIS\sFORECLOSURE\sSALE\sHAS\sBEEN\sCONTINUED\sTO\s)((January|February|March|April|May|June|July|August|September|October|November|December).{0,10}\s20\d\d)/gi;
     var re4 = /(?:THIS\sFORECLOSURE\sSALE\sHAS\sBEEN\sCONTINUED\sTO\s)(\d\d\/\d\d\/20\d\d)/gi;
     var re5 = /(?:THE\sREFERENCED\sFORECLOSURE\sSALE\sABOVE\sIS\sCONTINUED\sTO\s)((January|February|March|April|May|June|July|August|September|October|November|December).{0,10}\s20\d\d)/gi;
-    var results1 = re1.exec(str);
-    var results2 = re2.exec(str);
-    var results3 = re3.exec(str);
-    var results4 = re4.exec(str);
-    var results5 = re5.exec(str);
-    return results1 || results2 || results3 || results4 || results5;
-}
+
+    return (
+        re1.exec(str) ||
+        re2.exec(str) ||
+        re3.exec(str) ||
+        re4.exec(str) ||
+        re5.exec(str)
+    );
+};
+
+var tally = {};
+tally.failures = 0;
+tally.postponed = 0;
+tally.nonpostponed = 0;
 
 listings.forEach(function (listing) {
     var postponed = parse.date.postponed(listing.body);
     var match;
 
     if (postponed) {
-        console.log(Array(30).join('--='))
-        console.log('POSTPONED');
-        console.log(postponed[1])
+        tally.postponed++;
     } else {
         match = parse.date.strict(listing.body);
-
         if (match) {
-            console.log('!', match[1]);
-            if (match[1][match[1].length - 1] == 4) {
-                console.log(listing.body)
-            }
+            tally.nonpostponed++;
         } else {
-            console.log(Array(30).join('--='))
-            console.log('ERROR', match)
-            console.log(listing.body)
+            tally.failures++;
         }
     }
 });
+
+var successes = tally.postponed + tally.nonpostponed;
+console.log('successes:', successes);
+console.log('postponed:', tally.postponed);
+console.log('nonpostponed:', tally.nonpostponed);
+console.log('failures:', tally.failures);
