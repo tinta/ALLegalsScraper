c3943ff9b6edcd0087e57ed7ac1844ec86ebd7a5
whroman <me@walterroman.com>
2018-02-02 06:24:33 +0100

    it works!

--- scraper/index.js
+++ scraper/index.js
@@ -7,7 +7,6 @@ var Nightmare = require('nightmare')
 var _ = require('lodash')
 var moment = require('moment')
 var squel = require('squel').useFlavour('mysql')
-var Q = require('q')
 
 // Custom scripts
 var db = require('./../common/db-connect.js')()
@@ -29,93 +28,109 @@ const searchButtonId = '#ctl00_ContentPlaceHolder1_as1_btnGo'
 const countyFilterId = '#ctl00_ContentPlaceHolder1_as1_divCounty'
 const searchTypeSelector = '#ctl00_ContentPlaceHolder1_as1_rdoType_1'
 
+
+const oneSecondInMS = 1000
+const oneMinuteInMS = 60 * oneSecondInMS
 // Initializations
 const page = new Nightmare({
     show: false,
-    executionTimeout: 60 * 5 * 1000
+    executionTimeout: oneMinuteInMS * 10
 })
 
-const foo = [24, 35]
+const foo = [47, 51]
 const scrapeCounties = (counties) => {
-    const scrapedCounties = counties.reduce(
-        (fn, countyID) => fn.then(() => {
-            scrapeCounty(countyID)
-                .then((foreclosures) => writeToDB(foreclosures))
-        }), Q()
-    )
-    scrapedCounties.then(() => {
-        console.log('Finished scraping.')
-        db.end()
-    })
-}
-
-scrapeCounties(foo)
-// scrapeCounty(0);
-
-function scrapeCounty(countyID) {
     const xvfb = new Xvfb({
         silent: true
     })
     xvfb.startSync()
+
+    const scrapedCounties = counties.reduce(
+        (acc, countyID) => (
+            acc.then(() =>
+                scrapeCounty(countyID)
+                    .then((foreclosures) => writeToDB(foreclosures))
+            )
+
+        ), Promise.resolve([])
+    )
+    scrapedCounties.then(() => {
+        console.log('Finished scraping.')
+        db.end()
+        page.end()
+        xvfb.stopSync()
+    })
+}
+
+scrapeCounties(foo)
+
+function scrapeCounty(countyID) {
     console.log('Scraping county: ' + countyID)
     return page
+        .on('console', (log, msg) => {
+            console.log(msg)
+        })
         .goto(SCRAPE_URL)
         .type(searchBoxInputId, foreclosureSearchText)
         .click(searchTypeSelector)
         .click(countyFilterId)
         .click(countySelectorIdPrefix + countyID)
-        .wait(1000)
+        .wait(oneSecondInMS)
         .click(searchButtonId)
         /* a better solution waits for elem to load,
            but the html source doesn't change much after the page load :(
            just the css changes (e.g. hidden: true)
         */
-        .wait(6000)
+        .wait(oneSecondInMS * 6)
         .select('.select-page', '50') // change this to 5 for debugging
-        .wait(1000)
-        .evaluate(function(scrapeURL) {
+        .wait(oneSecondInMS)
+        .evaluate(function() {
             var foreclosures = {}
             var $tables = $('table.nested')
+            console.log($tables.length + ' entries were found.')
             $tables.each(function(i, table) {
                 var foreclosure = {}
                 var text = table.innerText.split('\n')
+                foreclosure.pubDate = text[1].match(', (.*)City')[1].trim() // Wednesday, January 17, 2018City: Birmingham
+                var oneWeekAgo = new Date()
+                oneWeekAgo.setDate(oneWeekAgo.getDate() - 2)
+                if (new Date(foreclosure.pubDate) < oneWeekAgo) return null
                 // javascript:location.href='Details.aspx?SID=dw44xhammmz1uuoskx0odpgv&ID=1830441';return false;
                 foreclosure.link = (table.rows[0].cells[0].children[0].onclick + '')
                     .split('href=\'')[1]
                     .split('\';return')[0]
                 foreclosure.caseId = foreclosure.link.split('&ID=')[1]
                 foreclosure.county = text[2].match(': (.*)')[1].trim() // County: Jefferson
-                foreclosure.pubDate = text[1].match(', (.*)City')[1].trim() // Wednesday, January 17, 2018City: Birmingham
                 foreclosure.source = text[0].trim() //    Alabama Messenger
                 $.ajax({
-                    url: scrapeURL + foreclosure.link,
+                    url: 'https://www.alabamapublicnotices.com/' + foreclosure.link,
                     type: 'GET',
                     async: false,
                     cache: false,
                     timeout: 30000,
-                }).then(function(data) {
-                    foreclosure.body = $(data.responseText)
-                        .find('#ctl00_ContentPlaceHolder1_PublicNoticeDetailsBody1_lblContentText')
-                        .text().trim()
-
-                    foreclosures[foreclosure.caseId] = foreclosure
-                }, function() {
-                    foreclosures[foreclosure.caseId] = null
+                    complete: function(data) {
+                        foreclosure.body = $(data.responseText)
+                            .find('#ctl00_ContentPlaceHolder1_PublicNoticeDetailsBody1_lblContentText')
+                            .text().trim()
+                        foreclosures[foreclosure.caseId] = foreclosure
+                    }
                 })
             })
             return foreclosures
-        }, SCRAPE_URL)
-        .end()
-        .then(() => {
-            xvfb.stopSync()
-            return Q()                
+        })
+        .then((foreclosures) => {
+            console.log('Scraping from target site complete.')
+            console.log(JSON.stringify(foreclosures))
+            return Promise.resolve(foreclosures)              
+        }, (error) => {
+            console.log('Scraping error:')
+            console.log(error)
+            return Promise.reject()
         })
 }
 
-function writeToDB(listings) {
-    var writeToDBDeferred = Q.defer()
+const writeToDB = (listings) => new Promise((resolve) => {
     var uids = {}
-    uids.scraped = _.keys(listings.filter((listing) => listing))
+    uids.scraped = Object.keys(listings)
     uids.present = []
     uids.absent = []
     uids.sql = []
@@ -135,8 +150,7 @@ function writeToDB(listings) {
 
     if (uids.scraped.length === 0) {
         console.log('No listings were found in this county')
-        writeToDBDeferred.resolve(false)
-        return writeToDBDeferred.promise
+        return resolve(false)
     }
 
     uids.scraped.forEach(function(uid, index) {
@@ -166,31 +180,20 @@ function writeToDB(listings) {
     query.on('end', function() {
         uids.absent = _.difference(uids.scraped, uids.present)
 
-        var deferred = Q.defer()
-        var loop = 0
+        let promise
 
         if (uids.absent.length == 0) {
             console.log('No new results were scraped')
-            deferred.resolve(true)
+            promise = Promise.resolve(true)
         } else {
             console.log('Fetched all results!')
             console.log(uids.scraped.length + ' listings were scraped.')
             console.log('Commencing DB inserts for following ' + uids.absent.length + ' listings, which were not found in our DB.')
             console.log(uids.absent.join(', '))
 
-            uids.absent.forEach(function(absentID) {
+            promise = uids.absent.reduce((acc, absentID) => acc.then(() => {
                 console.log('Commencing insertion for Foreclosure #' + absentID)
                 var absentForeclosure = listings[absentID]
-
-                if (!absentForeclosure) {
-                    console.log('Missing - Foreclosure #' + absentID + ' ' + loop)
-                    loop++
-                    if (loop == uids.absent.length) {
-                        loop++
-                        if (loop == uids.absent.length) deferred.resolve(true)
-                        return
-                    }
-                }
 
                 // elminate double spaces
                 var body = absentForeclosure.body
@@ -210,9 +213,14 @@ function writeToDB(listings) {
                     .where('body LIKE ?', insertMap['body'])
                     .toString()
 
-                sql.promise(sqlFindDuplicates).then(function(duplicates) {
-                    if (!util.isPresent(duplicates[0])) {
-                        console.log('No Duplicates Found - Foreclosure #' + absentID + ' ' + loop)
+                return sql.promise(sqlFindDuplicates)
+                    .then(function(duplicates) {
+                        const duplicatesFound = util.isPresent(duplicates[0])
+                        if (duplicatesFound) {
+                            console.log('Duplicate Found - Foreclosure #' + absentID)
+                            count.duplicates += 1
+                            return Promise.resolve(false)
+                        }
 
                         insertMap['case_id'] = parseInt(absentForeclosure.caseId)
                         insertMap['county'] = absentForeclosure.county
@@ -234,30 +242,26 @@ function writeToDB(listings) {
                             .toString()
 
                         return sql.promise(sqlInsertListing)
-                    }
-
-                    console.log('Duplicate Found - Foreclosure #' + absentID + ' ' + loop)
-                    count.duplicates += 1
-                    return Q(false)
-                })
-                    .then(function(insertObj) {
-                        if (insertObj) {
+                    })
+                    .then((insertedRow) => {
+                        if (insertedRow) {
                             console.log('Finished inserting Foreclosure #' + absentID)
                             count.inserts++
                         }
-                        loop++
-                        if (loop == uids.absent.length) deferred.resolve(true)
+                        return Promise.resolve()
+                    }, (error) => {
+                        console.log(error)
+                        console.log(absentForeclosure)
+                        return Promise.reject()
                     })
-            })
+            }), Promise.resolve(true))
         }
 
-        deferred.promise.then(function() {
+        promise.then(() => {
             count.print()
-            writeToDBDeferred.resolve(true)
-        }, function(err) {
+            resolve(true)
+        }, (err) => {
             throw err
         })
     })
-
-    return writeToDBDeferred.promise
-}
+})
